0a28d116d9cde4587260c073d5e04f06f6b5afc6
Hyde Francois Khamsing <66337110+Muzika-Senpai@users.noreply.github.com>
2022-07-06 12:14:15 +0200

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -2,21 +2,35 @@ var loadGame = document.querySelector(".navigation");
 var start = document.querySelector(".start-container");
 var header = document.querySelector(".header");
 var main = document.querySelector(".main");
+
+var selectors = document.querySelectorAll(".selector");
+
 var bgmStart = document.querySelector(".startBGM");
 
 function load(){
 
   header.classList.remove("hidden");
+  loadGame.classList.add("hidden");
   document.getElementById("my_audio").play();
 
 }
 
 function startGame(){
 
-    header.classList.add('hidden');
-    main.classList.remove('hidden');
-    document.getElementById("my_audio").pause();
-    document.getElementById("selection").play();
+  header.classList.add('hidden');
+  main.classList.remove('hidden');
+  document.getElementById("my_audio").pause();
+  document.getElementById("selection").play();
+
+}
+
+function selectCharacter(){
+
+  for( selector of selectors ){
+
+    console.log(selector);
+
+  }
 
 }
 

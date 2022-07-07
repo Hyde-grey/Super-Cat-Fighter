57681065172e4ad2abcd9f5fb7d78aa074aaf556
Hyde Francois Khamsing <66337110+Muzika-Senpai@users.noreply.github.com>
2022-07-07 10:50:51 +0200

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,30 @@
+var consoleStart = document.querySelector(".console-start");
 var loadGame = document.querySelector(".navigation");
 var start = document.querySelector(".start-container");
 var header = document.querySelector(".header");
 var main = document.querySelector(".main");
+var bgmStart = document.getElementById("main-bgm");
+var intro = document.querySelector(".intro");
 
-var bgmStart = document.querySelector(".startBGM");
+var nIntervID;
 
 var current = 1;
+
+function loadBGM(){
+
+  bgmStart.play();
+  consoleStart.classList.add("hidden");
+  start.classList.remove("hidden");
+
+}
 
 function load(){
 
   header.classList.remove("hidden");
   loadGame.classList.add("hidden");
-  document.getElementById("my_audio").play();
+  intro.play();
+  nIntervID = setInterval(loadBGM, 3000);
+  
 
 }
 
@@ -19,8 +32,12 @@ function startGame(){
 
   header.classList.add('hidden');
   main.classList.remove('hidden');
-  document.getElementById("my_audio").pause();
+
+  bgmStart.pause();
   document.getElementById("selection").play();
+
+  clearInterval(nIntervID);
+  nIntervID = null;
 
 }
 
